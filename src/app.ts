a4185a16695d308524b23cf4d9c72778ec627cd0
yalab <rudeboyjet@gmail.com>
2024-03-09 17:01:42 +0900

    Started to implement bolt app.

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-console.log('Hello World!')
+import { App } from '@slack/bolt';
+import 'dotenv/config'
+
+const app = new App({"token": process.env.SLACK_BOT_TOKEN || '',
+		     "signingSecret": process.env.SLACK_SIGNING_SECRET || ''});
+
+(async () => {
+  await app.start(process.env.PORT || 3000);
+
+  console.log('⚡️ Bolt app is running!');
+})();
