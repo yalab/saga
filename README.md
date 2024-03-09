d97c0f65c3bae0efa01879db0c7a293f688cf882
yalab <rudeboyjet@gmail.com>
2024-03-09 17:26:42 +0900

    Add Slack App token and socket mode, subscribe to events

--- README.md
+++ README.md
@@ -6,4 +6,14 @@ This is a Slack bot for talking to Google Gemini.
 
 SLACK_BOT_TOKEN: 'Basic Information'
 SLACK_SIGNING_SECRET: 'OAuth & Permissions'
+SLACK_APP_TOKEN: 'Socket Mode'
+
+## Event Subscriptions
+
+### Subscribe to bot events
+
+- message.channels
+- message.groups
+- message.im
+- message.mpim
 
--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,18 @@ import { App } from '@slack/bolt';
 import 'dotenv/config'
 
 const app = new App({"token": process.env.SLACK_BOT_TOKEN || '',
-		     "signingSecret": process.env.SLACK_SIGNING_SECRET || ''});
+		     "signingSecret": process.env.SLACK_SIGNING_SECRET || '',
+		     "appToken": process.env.SLACK_APP_TOKEN,
+		     "socketMode": true,
+		     "port": Number(process.env.PORT) || 3000});
+
+app.message('hello', async ({ message, say }) => {
+  console.log(message)
+  await say(`Hey guys!`);
+});
+
 
 (async () => {
-  await app.start(process.env.PORT || 3000);
-
+  await app.start();
   console.log('⚡️ Bolt app is running!');
 })();
